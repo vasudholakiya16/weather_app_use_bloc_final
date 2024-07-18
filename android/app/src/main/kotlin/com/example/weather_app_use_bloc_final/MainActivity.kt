b75643822503646655b68d325a69b18682471240
vasudholakiya16 <122475789+vasudholakiya16@users.noreply.github.com>
2024-07-18 22:49:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/weather_app_use_bloc_final/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weather_app_use_bloc_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/my_data.dart
@@ -0,0 +1,1 @@
+String API_KEY = "a250dcf7dcaaca25d4a44ac2c595cb4c";
